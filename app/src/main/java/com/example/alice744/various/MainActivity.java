ad80dc8a9737b010a86da46596e460179a7b771d
alice744 <alice.winner74@gmail.com>
2018-10-27 15:35:16 +0700

    fix lorry 1

--- app/src/main/java/com/example/alice744/various/MainActivity.java
+++ app/src/main/java/com/example/alice744/various/MainActivity.java
@@ -6,5 +6,6 @@ import com.example.alice744.various.car.Lorry;
 public class MainActivity  {
     public static void main(String[] args) { ;
     Lorry lorry=new Lorry("a",8,9);
+    Lorry lorry1=new Lorry("null",0,0);
     }
 }
